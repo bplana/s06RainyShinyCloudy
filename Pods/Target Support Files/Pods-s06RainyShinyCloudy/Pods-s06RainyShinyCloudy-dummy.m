d5d1585b69cabd4ca4b712cf6efd3aeafdd2d5d7
BP <oh_strawberry@yahoo.com>
2017-03-01 13:40:02 -0800

    begin & added ui

---
+++ Pods/Target Support Files/Pods-s06RainyShinyCloudy/Pods-s06RainyShinyCloudy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_s06RainyShinyCloudy : NSObject
+@end
+@implementation PodsDummy_Pods_s06RainyShinyCloudy
+@end
---
+++ s06RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,54 @@
+//
+//  WeatherVC.swift
+//  s06RainyShinyCloudy
+//
+//  Created by bernadette on 2/28/17.
+//  Copyright © 2017 Bernadette P. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // set delegate & dataSource
+        tableView.delegate = self
+        tableView.dataSource = self
+    
+    }
+
+    // tableView delegate funcs:
+    // (1)  numberOfSections in tableView
+    // (2)  numberOfRowsInSection (how many cells do you want in the tableView)
+    // (3)  cellForRowAt indexPath
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6        // for the rest of the days of the week
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // need to create a cell (dequeueReusableCell (withIdentifier.. for..))
+        // -- also need to give storyboard's Table View Cell an identifier, for "withIdentifier" string (below)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+
+}
+
